c187cdf978f51e7c4a04f494243c9a6be2c1ad40
Sidnei Eiji Nishiyama <sidnei.qa@gmail.com>
2016-11-18 09:53:55 -0200

    Adding new scenario

--- features/pages/agency.rb
+++ features/pages/agency.rb
@@ -1,8 +1,44 @@
 class Agency
 	include Capybara::DSL
 	
+	def last_window
+		page.driver.browser.switch_to.window(page.driver.browser.window_handles.last)	
+	end
+
 	def click_something(link)
 		click_link(link)
+	end
+
+	def click_search_postalcode
+		within_frame(find("iframe")) do
+			find(:css,'#BuscaAgenProximaForm > ul.botoes > li.alignR > a > img').click
+		end
+	end
+
+	def click_search_neighborhood
+		within_frame(find("iframe")) do
+			find(:css,'#BuscaAgenBairro > table > tbody > tr > td > ul.botoes > li.alignR > a > img').click
+		end
+	end
+
+	def click_search_agencyNumber
+		within_frame(find("iframe")) do
+			find(:css,'#BuscaAgenNumero > table > tbody > tr > td > ul > li.alignR > a > img').click
+		end
+	end
+
+	def click_agency_neighborhood
+		last_window
+		within_frame(find("iframe")) do 
+			find("#OpcaoBuscaAgenBairro").click
+		end
+	end
+
+	def click_agency_number
+		last_window
+		within_frame(find("iframe")) do 
+			find("#OpcaoBuscaAgenNumero").click
+		end
 	end
 
 	def fill_cep
@@ -12,19 +48,10 @@ class Agency
 		end
 	end
 
-	def last_window
-		page.driver.browser.switch_to.window(page.driver.browser.window_handles.last)	
-	end
-
-	def click_search
-		within_frame(find("iframe")) do
-			find(:css,'#BuscaAgenProximaForm > ul.botoes > li.alignR > a > img').click
-		end
-	end
-
-	def click_search_agency
-		within_frame(find("iframe")) do
-			find(:css,'#BuscaAgenBairro > table > tbody > tr > td > ul.botoes > li.alignR > a > img').click
+	def fill_agency_number
+		last_window
+		within_frame(find("iframe")) do 
+			fill_in "txNumeroAgencia", :with => "1174"
 		end
 	end
 
@@ -34,26 +61,23 @@ class Agency
 		end
 	end
 
-	def verify_content
-		page.has_content?('1744 - SELECT ITAIM BIBI-SP')
-	end
-
-	def verify_result
-		page.has_content?('1556 - BOSQUE-RIO BRANCO-AC')
-	end
-
-	def click_agency
-		last_window
-		within_frame(find("iframe")) do 
-			find("#OpcaoBuscaAgenBairro").click
-		end
-	end
-
 	def select_data
 		within_frame(find("iframe")) do
 			find('#localizacaoEstado').find(:xpath,'option[2]').select_option
 			find('#localizacaoCidade').find(:xpath,'option[2]').select_option
 			find('#localizacaoBairro').find(:xpath,'option[2]').select_option
 		end
+	end
+
+	def verify_content_postalcode
+		page.has_content?('1744 - SELECT ITAIM BIBI-SP')
+	end
+
+	def verify_content_neighborhood
+		page.has_content?('1556 - BOSQUE-RIO BRANCO-AC')
+	end
+
+	def verify_content_agencyNumber
+		page.has_content?('1174 - SANTO ANGELO - RS')
 	end
 end
--- features/step_definitions/agency_steps.rb
+++ features/step_definitions/agency_steps.rb
@@ -11,30 +11,46 @@ Quando(/^preencher o campo CEP$/) do
   	sleep 2
 end
 
-Quando(/^clicar no botão buscar$/) do
-	Agency.new.click_search
+Quando(/^clicar no botão para buscar agência pelo CEP$/) do
+	Agency.new.click_search_postalcode
 end
 
-Quando(/^clicar no botão buscar agência$/) do
-	Agency.new.click_search_agency
+Quando(/^clicar no botão para buscar agência pelo bairro$/) do
+	Agency.new.click_search_neighborhood
 end
 
 Quando(/^selecionar um endereço$/) do
  	Agency.new.select_address
 end
 
-Então(/^aparecerá as agências disponíveis$/) do
-	Agency.new.verify_content
+Então(/^aparecerá as agências disponíveis do CEP$/) do
+	Agency.new.verify_content_postalcode
 end
 
-Quando(/^clicar na aba Busque Agência no Bairro$/) do
-  	Agency.new.click_agency
+Quando(/^clicar na aba para buscar agência no bairro$/) do
+  	Agency.new.click_agency_neighborhood
 end
 
 Quando(/^selecionar os dados obrigatórios$/) do
   	Agency.new.select_data
 end
 
-Então(/^aparecerá as agências disponíveis na aba Busque Agência no Bairro$/) do
-  	Agency.new.verify_result
+Então(/^aparecerá as agências disponíveis do bairro$/) do
+  	Agency.new.verify_content_neighborhood
+end
+
+Quando(/^clicar na aba para buscar agência pelo número$/) do
+ 	Agency.new.click_agency_number
+end
+
+Quando(/^preencher os dados obrigatórios$/) do
+ 	Agency.new.fill_agency_number
+end
+
+Quando(/^clicar no botão para buscar agência pelo número$/) do
+ 	Agency.new.click_search_agencyNumber
+end
+
+Então(/^aparecerá a agência solicitada$/) do
+	Agency.new.verify_content_agencyNumber
 end
